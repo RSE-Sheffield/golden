c3242786e2e46925593c670f786053b35e820cd6
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2025-11-18 12:03:49 +0000

    Remove all the cpp input validation, this is now defined in R

--- src/rcpp_interface.cpp
+++ src/rcpp_interface.cpp
@@ -175,122 +175,25 @@ inline void check_trajectory_contains(List h, const int i, const std::string &na
 
 // [[Rcpp::export]]
 List run_simulation(List initPop, List parms) {
-    // Load parameters, or use default if not present
-    if (!parms.containsElementNamed("steps"))
-        throw std::invalid_argument("List 'parms' expected to contain integer 'steps'\n");
-    const int STEPS = parms.containsElementNamed("steps") ? parms["steps"] : 1;
-    const uint64_t RANDOM_SEED = parms.containsElementNamed("random_seed") ? static_cast<uint64_t>(parms["random_seed"]) : 2999569345;
+    // Call eldoradosim::check_parameters()
+    {        
+        Environment eldoradosim = Environment::namespace_env("eldoradosim");
+        Function check_parameters = eldoradosim["check_parameters"];
+        check_parameters(parms);
+    }
+    // Extract configuration options
+    const int STEPS = parms["steps"];
+    const uint64_t RANDOM_SEED = static_cast<uint64_t>(parms["random_seed"]);
 #ifdef NDEBUG
-    const bool DEBUG = parms.containsElementNamed("debug") ? static_cast<bool>(parms["debug"]) : false;
+    const bool DEBUG = static_cast<bool>(parms["debug"]);
     warning("eldoradosim's model debug checks are enabled, these may impact performance.");
 #else
     const bool DEBUG = true;
     warning("You are using a development build of eldoradosim, this may impact performance.");
 #endif
-    std::set<std::string> special_args = {"~STEP"};
-    // Validate initPop has columns required by hazard functions
-    if (!parms.containsElementNamed("hazards"))
-        throw std::invalid_argument("List 'parms' expected to contain vector 'hazards'\n");
+    const std::set<std::string> special_args = {"~STEP"};
     List hazards = parms["hazards"];
-    if (!hazards.length())
-        throw std::invalid_argument("'parms$hazards' is empty\n");
-    for (int h_i = 0; h_i < Rf_length(hazards); ++h_i) {
-        List h = hazards[h_i];
-        // Check hazard actually contains required elements
-        // @todo Can we instead define an R type which would be mostly default init?
-        check_hazard_contains(h, h_i, "parms", "vector");
-        check_hazard_contains(h, h_i, "fn", "function");
-        check_hazard_contains(h, h_i, "transitions", "vector");
-        StringVector hazardParms = h["parms"];
-        // Check that listed parameters are found inside initPop columns
-        for (const String hp : hazardParms) {
-            const std::string hp_string = hp.get_cstring();
-            if (hp_string.length() > 1 && hp_string[0] == '~') {
-                // Special internal arg
-                if (special_args.find(hp_string) == special_args.end()) {
-                    std::stringstream err;
-                    err << "Unrecognised special hazard arg '" << hp_string <<"'.\n";
-                    throw std::invalid_argument(err.str());
-                }
-            } else {
-                // Arg from population datatable
-                if(!initPop.containsElementNamed(hp_string.c_str())) {
-                    std::stringstream err;
-                    err << "Column '" << hp_string <<"' expected inside data.table 'initPop'\n";
-                    throw std::invalid_argument(err.str());
-                }
-            }
-        }
-        List transition_list = h["transitions"];
-        for (int t_i = 0; t_i < Rf_length(transition_list); ++t_i) {
-            List transit = transition_list[t_i];
-            check_transition_contains(transit, h_i, t_i, "parms", "vector");
-            check_transition_contains(transit, h_i, t_i, "fn", "function");
-            check_transition_contains(transit, h_i, t_i, "state", "string");
-            hazardParms = transit["parms"];
-            // Check that listed transition parameters are found inside initPop columns
-            for (const String hp : hazardParms) {
-                const std::string hp_string = hp.get_cstring();
-                if (hp_string.length() > 1 && hp_string[0] == '~') {
-                    // Special internal arg
-                    if (special_args.find(hp_string) == special_args.end()) {
-                        std::stringstream err;
-                        err << "Unrecognised special hazard transition arg '" << hp_string <<"'.\n";
-                        throw std::invalid_argument(err.str());
-                    }
-                } else {
-                    // Arg from population datatable
-                    if(!initPop.containsElementNamed(hp_string.c_str())) {
-                        std::stringstream err;
-                        err << "Column '" << hp_string <<"' expected inside data.table 'initPop'\n";
-                        throw std::invalid_argument(err.str());
-                    }
-                }
-            }
-            // Confirm transition_state state exists in initPop
-            String ts = transit["state"];
-            if(!initPop.containsElementNamed(ts.get_cstring())) {
-                std::stringstream err;
-                err << "Transition state column '" << ts.get_cstring() <<"' expected inside data.table 'initPop'\n";
-                throw std::invalid_argument(err.str());
-            }            
-        }
-    }
-    // Validate initPop has columns required by trajectory functions
-    if (!parms.containsElementNamed("trajectories"))
-        throw std::invalid_argument("List 'parms' expected to contain vector 'trajectories'\n");
     List trajectories = parms["trajectories"];
-    if (!trajectories.length())
-        throw std::invalid_argument("'parms$trajectories' is empty\n");
-    for (int t_i = 0; t_i < Rf_length(trajectories); ++t_i) {
-        List t = trajectories[t_i];
-        // Check hazard actually contains required elements
-        // @todo Can we instead define an R type which would be mostly default init?
-        check_trajectory_contains(t, t_i, "parms", "vector");
-        check_trajectory_contains(t, t_i, "fn", "function");
-        check_trajectory_contains(t, t_i, "property", "string");
-        StringVector trajParms = t["parms"];
-        // Check that listed parameters are found inside initPop columns
-        for (const String tp : trajParms) {
-            const std::string tp_string = tp.get_cstring();
-            if (tp_string.length() > 1 && tp_string[0] == '~') {
-                // Special internal arg
-                if (special_args.find(tp_string) == special_args.end()) {
-                    std::stringstream err;
-                    err << "Unrecognised special trajectory arg '" << tp_string <<"'.\n";
-                    throw std::invalid_argument(err.str());
-                }
-            } else {
-                // Arg from population datatable
-                if(!initPop.containsElementNamed(tp_string.c_str())) {
-                    std::stringstream err;
-                    err << "Column '" << tp_string <<"' expected inside data.table 'initPop'\n";
-                    throw std::invalid_argument(err.str());
-                }
-            }
-        }        
-    }
-    // @todo Validate data table has other mandatory columns
     
     // Create a deep-copy of the initial pop, that we will return with changes at the end
     // @todo create other columns, to log last hazard score?
