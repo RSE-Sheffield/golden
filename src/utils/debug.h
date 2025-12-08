8ee36bb4e54a2a1ad0d81c6350ec29713eba8b3d
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2025-12-08 13:38:20 +0000

    All classes with member var "fn" now have member var "name"
    This is an optional member var which can be manually set by the user.
    If not, in the first instance it will be replaced with name of variable passed to "fn".
    If a literal is passed to "fn", instead the index of the hazard/transition/trajectory is strigified.
    
    This name will be used to identify the relevant object, when reporting warnings/errors related to return values from user functions.

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -12,53 +12,57 @@ using namespace Rcpp;
  * Debug method to check output of dynamic methods, to ensure they don't contain NaN/Inf
  * 
  * @param s_i Step index
- * @param fn_type String representation of the function type (e.g. hazard, trajectory, transition)
- * @param fn_i Function index, e.g. 2nd hazard in the list. Assumed 1-indexed same as R, rather than 0-indexed C
+ * @param name String representation of the function type (e.g. hazard[[1]$transitions[[2]])
  * @param _result The result vector to be checked, assumed to be NumericVector (only floating point types have NaN)
- * @param parent_typ (Optional) For transition functions, this will be set to "hazard"
- * @param p_i (Optional) Index of the parent type (hazard). Assumed 1-indexed same as R, rather than 0-indexed C
  * @note Not sure R has a concept of Inf, everything so far seems to be NA, which I assume to be NaN
- * @note Labelling may need adjustments if allowing multiple transitions per hazard
  */
-void check_result(int s_i, const std::string &fn_typ, int fn_i, SEXP _result, const std::string &parent_typ = "", int p_i = 0) {
+void check_result(int s_i, const std::string &name, SEXP _result) {
     if (Rf_isNumeric(_result)) {
         NumericVector result = _result;
         int nan_count = 0;
         int inf_count = 0;
-        int range_count = 0;
         for (const double &t : result) {
             nan_count += std::isnan(t)?1:0;
             inf_count += std::isinf(t)?1:0;
-            if (t < 0) {
-                ++range_count;
-            }
         }
         if (nan_count + inf_count) {
             // This should never be hit
             std::stringstream err;
-            err << "[DEBUG]During step " << s_i << " " << fn_typ << "[" << fn_i << "] ";
-            if (!parent_typ.empty()) {
-                err << "from " << parent_typ << "[" << p_i << "] ";
-            }
-            err << "return contained " << nan_count <<" NaN values and " << inf_count << " Inf values.\n";
+            err << "[DEBUG]During step " << s_i << " " << name;
+            err << " return contained " << nan_count <<" NaN values and " << inf_count << " Inf values.\n";
             throw std::runtime_error(err.str());
         }
-        if (fn_typ == "hazard" && range_count) {
+    }
+}
+/**
+ * Extended variant with additional hazard only checks
+ * @see check_result(int, const std::string&, SEXP)
+ */
+void check_hazard_result(int s_i, const std::string &name, SEXP _result) {
+    if (Rf_isNumeric(_result)) {
+        NumericVector result = _result;
+        // R
+        check_result(s_i, name, _result);
+        int range_count = 0;
+        for (const double &t : result) {
+            if (t < 0) {
+                ++range_count;
+            }
+        }
+        if (range_count) {
             // Special case, warn if hazard return is <0
             std::stringstream err;
-            err << "[DEBUG]During step " << s_i << " " << fn_typ << "[" << fn_i << "] ";
-            err << "return contained " << range_count <<" negative values.\n";
+            err << "[DEBUG]During step " << s_i << " " << name;
+            err << " return contained " << range_count <<" negative values.\n";
             err << "Hazards are expected to return values greater than or equal to 0\n";
             warning(err.str());
         }
     } else {
-        if (fn_typ == "hazard") {
-            // This should never be hit
-            std::stringstream err;
-            err << "[DEBUG]During step " << s_i << " " << fn_typ << "[" << fn_i << "] ";
-            err << "return was type " << type2name(_result) << " expected type double\n";
-            throw std::runtime_error(err.str());
-        }
+        // This should never be hit
+        std::stringstream err;
+        err << "[DEBUG]During step " << s_i << " " << name;
+        err << " return was type " << type2name(_result) << " expected type double\n";
+        throw std::runtime_error(err.str());
     }
 }
 
