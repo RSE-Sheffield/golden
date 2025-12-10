554948bccbcb1816f9381a5394582414fb228a5d
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2025-12-10 13:43:28 +0000

    Refactor into object-orientated structure.
    This enables slightly cleaner code.
    
    Could go further, to reduce args to debug/util headers.

---
+++ src/Simulation.cpp
@@ -0,0 +1,241 @@
+#include "Simulation.h"
+
+#include "utils/dynamic_call.h"
+#include "utils/debug.h"
+
+
+const std::set<std::string> Simulation::SPECIAL_ARGS = {"~STEP"};
+
+int initHistoryFreq(List parameters) {
+    List history = parameters.containsElementNamed("history") ? parameters["history"] : R_NilValue;
+    if (history.size())
+        return history["frequency"];
+    return 0;
+}
+int initHistoryRows(List parameters) {
+    List history = parameters.containsElementNamed("history") ? parameters["history"] : R_NilValue;
+    if (history.size())
+        return static_cast<int>(parameters["steps"]) / static_cast<int>(history["frequency"]);
+    return 0;
+}
+
+Simulation::Simulation(List parameters)
+    : STEPS(parameters["steps"])
+    , RANDOM_SEED(parameters["random_seed"])
+#ifdef NDEBUG
+    , DEBUG(static_cast<bool>(parameters["debug"]))
+#else
+    , DEBUG(true)
+#endif
+    , HISTORY_FREQ(initHistoryFreq(parameters))
+    , HISTORY_ROWS(initHistoryRows(parameters))
+    , columns(R_NilValue)
+    , population(R_NilValue)
+    , historyLog(List::create()){
+    hazards = parameters["hazards"];
+    trajectories = parameters["trajectories"];
+    history = parameters.containsElementNamed("history") ? parameters["history"] : R_NilValue;
+    // Extract configuration options
+    if (this->RANDOM_SEED) {
+        // R uses a global random state, however if a user provides a non-zero seed we will override that state
+        Function set_seed("set.seed");
+        set_seed(this->RANDOM_SEED );
+    }
+    if (DEBUG) {
+        warning("eldoradosim's model debug checks are enabled, these may impact performance.");
+    }
+    // Init History columns (difficult to do this in init list as requires assignment)
+    if (history.size()) {
+        columns = history["columns"];
+    }
+}
+
+List Simulation::run(List initPop) {
+    // Create a deep-copy of the initial pop, that we will return with changes at the end
+    // @todo create other columns, to log last hazard score?
+    population = clone(initPop);
+        
+    // Init History Data-table
+    if (history.size()) {
+        historyLog = List::create();
+        IntegerVector stepv = IntegerVector(HISTORY_ROWS, 0);
+        for (int i = 0; i < HISTORY_ROWS; ++i) {
+            stepv[i] = (i * HISTORY_FREQ) + 1;
+        }
+        historyLog["~STEP"] = stepv;
+    }
+    
+    // Simulation loop
+    for (step = 0; step < STEPS; ++step) {
+        stepHazards();
+        stepTrajectories();
+        stepHistory();
+        printf("\rStep %d/%d complete", step + 1, STEPS);
+    }
+    printf("\rSimulation Complete!\n");
+    
+    return buildOutput();
+}
+
+void Simulation::stepHazards() {
+    int h_i = 1;  // 1 index'd similar to R
+    // Execute hazards in order, if required at current step
+    for (List hazard : hazards) {
+        // If hazard is active this step
+        const unsigned int freq = hazard["freq"];
+        const int before = hazard["last"];
+        const int after = hazard["first"];
+        if(step % freq == 0 && step <= before && step >= after) {
+            // Build arg list to execute hazard chance
+            List call_args = build_args(hazard["args"], population, step);
+            // Execute hazard function and process results
+            // p=1-exp(-h*dt)
+            // p = probability
+            // h = hazard result
+            // dt = 1 (could vary in practice, but currently fixed)
+            // if rng < p for an agent, then transition function is applied
+            // hence p >= 1 is guaranteed, p > 1 potentially erroneous
+            NumericVector h = dynamic_call(hazard["fn"], call_args);
+            const int dt = 1;
+            NumericVector p = 1 - exp(-h * dt);
+            NumericVector rng = runif(Rf_length(h));  // Generate vector of random float [0, 1)
+            if (DEBUG)
+                check_hazard_result(step, hazard["name"], h);
+            // Process hazard's transitions
+            List transition_list = hazard["transitions"];
+            int t_i = 1;  // 1 index'd similar to R
+            for(List transition : transition_list) {
+                // Build arg list to execute hazard transition
+                call_args = build_args(transition["args"], population, step);
+                // @todo can this be hidden inside a util function cleanly?
+                // Only apply transitions in cases where hazard occurred
+                String ts_name = transition["state"];
+                // Select correct output based on output state type
+                if (Rf_isNumeric(population[ts_name])) {
+                    NumericVector transition_result = dynamic_call(transition["fn"], call_args);
+                    if (DEBUG)
+                        check_result(step, transition["name"], transition_result);
+                    NumericVector transition_state = population[ts_name];
+                    population[ts_name] = ifelse(rng < p, transition_result, transition_state);
+                } else if (Rf_isInteger(population[ts_name])) {
+                    IntegerVector transition_result = dynamic_call(transition["fn"], call_args);
+                    IntegerVector transition_state = population[ts_name];
+                    population[ts_name] = ifelse(rng < p, transition_result, transition_state);
+                } else if (Rf_isLogical(population[ts_name])) {
+                    LogicalVector transition_result = dynamic_call(transition["fn"], call_args);
+                    LogicalVector transition_state = population[ts_name];
+                    population[ts_name] = ifelse(rng < p, transition_result, transition_state);
+                } else {
+                    // @todo Support other types?(Complex, String, Date, Datetime)
+                    stop("Unsupported type at transition processing");
+                }
+                ++t_i;
+            }
+        }
+        ++h_i;
+    }
+}
+void Simulation::stepTrajectories() {
+    int t_i = 1; // 1-indexed counter
+    // Execute trajectories in order
+    for (List trajectory : trajectories) {
+        // Currently assumed that trajectories are always active
+        // Build arg list to execute trajectory chance
+        List call_args = build_args(trajectory["args"], population, step);
+        // Execute trajectory function and store result directly in trajectory's property
+        // Slightly different path, depending on whether it returns 1 or multiple properties
+        if (Rf_length(trajectory["property"]) == 1) {
+            // Single return value (returned list should be a column)
+            String trajectory_prop = trajectory["property"];
+            population[trajectory_prop] = dynamic_call(trajectory["fn"], call_args);
+            if (DEBUG)
+                check_result(step, trajectory["name"], population[trajectory_prop]);
+        } else {
+            // Multiple return values (returned list, should be a list of columns)
+            List trajectory_returns = dynamic_call(trajectory["fn"], call_args);
+            CharacterVector trajectory_properties = trajectory["property"];
+            if (Rf_length(trajectory_properties) != Rf_length(trajectory_returns))
+                stop("Trajectory function return value contains a different number of properties than expected.");
+            for (int i = 0; i < Rf_length(trajectory_properties); ++i) {
+                String trajectory_prop = trajectory_properties[i];
+                population[trajectory_prop] = trajectory_returns[i];
+                if (DEBUG) // Possibly want to make naming of which return vector clearer
+                    check_result(step, trajectory["name"], population[trajectory_prop]);
+            }
+        }
+        t_i++;
+    }
+}
+void Simulation::stepHistory() {
+    if (history.size()) {
+        // Is history desired this step
+        if (step % HISTORY_FREQ == 0) {
+            const unsigned int hist_i = step / HISTORY_FREQ;
+            // Calculate and store result for each column of output
+            for (List col : columns) {
+                String name = col["name"];
+                // Calculate filter vector if required by column
+                LogicalVector filter_v = LogicalVector();
+                if (col["filter_fn"] != R_NilValue) {
+                    List call_args = build_args(col["filter_args"], population, step);
+                    filter_v = dynamic_call(col["filter_fn"], call_args);
+                }
+                // Columns may be filtered here
+                List call_args = col["filter_fn"] != R_NilValue
+                    ? build_args_filtered(col["args"], population, step, filter_v)
+                    : build_args(col["args"], population, step);
+                // Call the dynamic function
+                SEXP result = dynamic_call(col["fn"], call_args);
+                if (hist_i == 0) {
+                    // Detect the type of the result and create corresponding vector
+                    if (Rf_isNumeric(result)) {
+                        historyLog[name] = NumericVector(HISTORY_ROWS, 0.0);
+                    } else if (Rf_isInteger(result)) {
+                        historyLog[name] = IntegerVector(HISTORY_ROWS, 0);
+                    } else if (Rf_isLogical(result)) {
+                        historyLog[name] = LogicalVector(HISTORY_ROWS, false);
+                    } else {
+                        // @todo Support other types?(Complex, String, Date, Datetime)
+                        stop("Unsupported type at history processing");
+                    }
+                }
+                // Store result
+                if (Rf_isNumeric(result)) {
+                    NumericVector t = historyLog[name];
+                    t[hist_i] = as<double>(result);
+                } else if (Rf_isInteger(result)) {
+                    IntegerVector t = historyLog[name];
+                    t[hist_i] = as<int>(result);
+                } else if (Rf_isLogical(result)) {
+                    LogicalVector t = historyLog[name];
+                    t[hist_i] = as<bool>(result);
+                } else {
+                    // @todo Support other types?(Complex, String, Date, Datetime)
+                    stop("Unsupported type at history processing");
+                }
+            }
+        }
+    } 
+}
+
+List Simulation::buildOutput() {
+    // Can only set as data.table once all columns have been added
+    
+    // Mark population as a data table
+    population.attr("class") = CharacterVector::create("data.table", "data.frame");
+    population.attr("row.names") = IntegerVector::create(NA_INTEGER, -Rf_length(population[0]));
+    if (history.size()) {
+        // Mark history as a data table
+        // (This must be done after we're finished adding columns)
+        historyLog.attr("class") = CharacterVector::create("data.table", "data.frame");
+        historyLog.attr("row.names") = IntegerVector::create(NA_INTEGER, -HISTORY_ROWS);
+        // Package it into a structure for return
+        List ret = List::create(
+            _["pop"]   = population,
+            _["history"] = historyLog
+        );
+        return ret;
+    } else {
+        return population;
+    }
+}
---
+++ src/Simulation.h
@@ -0,0 +1,46 @@
+#ifndef SIMULATION_H_
+#define SIMULATION_H_
+
+#include <set>
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+class Simulation {
+    static const std::set<std::string> SPECIAL_ARGS;
+    const int STEPS;
+    const int RANDOM_SEED;
+    const bool DEBUG;
+    const int HISTORY_FREQ;
+    const int HISTORY_ROWS;
+    // Param
+    List hazards;
+    List trajectories;
+    List history;
+    List columns;
+    // Runtime data
+    List population;
+    List historyLog;
+    int step = 0; // Simulation iteration index (0-indexed)
+    
+    void stepHazards();
+    void stepTrajectories();
+    void stepHistory();
+    
+    List buildOutput();
+    
+  public:
+    /**
+     * Initialise a simulation model
+     * @parameters Instance of R S3 class eldoradosim_parameters, which defines the simulation structure
+     */
+    Simulation(List parameters);
+    /**
+     * Run the simulation with the provided initial population
+     * @param initPop
+     * @return Either data.table containing final population, or list containing $pop the same data.table and $history the collected history.
+     */
+    List run(List initPop);
+};
+
+#endif  // SIMULATION_H_
