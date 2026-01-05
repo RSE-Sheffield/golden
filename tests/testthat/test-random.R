3c2c5d2756105e1ef75a7e2ec9ac67973290be0d
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2026-01-05 12:23:22 +0000

    Test that RNG affects RNG in user functions.

--- tests/testthat/test-random.R
+++ tests/testthat/test-random.R
@@ -69,3 +69,66 @@ test_that("Random==0, same random, same hazard results", {
     rng_12b = run_simulation(initPop, parms)
     expect_identical(rng_12a["pop"], rng_12b["pop"])
 })
+
+get_parms_user_rng <- function() {
+    return(
+        parms <- new_parameters(
+          hazards = list(
+            new_hazard(
+              always_hazard_fn, # Always returns Inf
+              c("a"),
+              list(
+                new_transition(transition_to_1_rng_fn, c("a"), "a")
+              )
+            )
+          ),
+          trajectories = list(
+            new_trajectory(empty_trajectory_fn, c("b"), "b")
+          ),
+          steps = 1
+        )
+    )
+}
+
+test_that("Different random, different user_fn results", {
+    N <- 100
+    initPop <- sample_pop2(N) # All init vars are 0
+    parms <- get_parms_user_rng()
+    parms$random_seed = 12
+    rng_12 = run_simulation(initPop, parms)
+    parms$random_seed = 13
+    rng_13 = run_simulation(initPop, parms)
+    expect_false(identical(rng_12["pop"], rng_13["pop"]))
+})
+test_that("Same random, same user_fn results", {
+    N <- 100
+    initPop <- sample_pop2(N) # All init vars are 0
+    parms <- get_parms_user_rng()
+    parms$random_seed = 12
+    rng_12a = run_simulation(initPop, parms)
+    parms$random_seed = 12
+    rng_12b = run_simulation(initPop, parms)
+    expect_identical(rng_12a["pop"], rng_12b["pop"])
+})
+test_that("Random==0, different random, different user_fn results", {
+    N <- 100
+    initPop <- sample_pop2(N) # All init vars are 0
+    parms <- get_parms_user_rng()
+    parms$random_seed = 0
+    set.seed(12)
+    rng_12 = run_simulation(initPop, parms)
+    set.seed(13)
+    rng_13 = run_simulation(initPop, parms)
+    expect_false(identical(rng_12["pop"], rng_13["pop"]))
+})
+test_that("Random==0, same random, same user_fn results", {
+    N <- 100
+    initPop <- sample_pop2(N) # All init vars are 0
+    parms <- get_parms_user_rng()
+    parms$random_seed = 0
+    set.seed(12)
+    rng_12a = run_simulation(initPop, parms)
+    set.seed(12)
+    rng_12b = run_simulation(initPop, parms)
+    expect_identical(rng_12a["pop"], rng_12b["pop"])
+})
