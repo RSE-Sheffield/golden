134800f7adc837faad63e7691705866350d0859c
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2026-03-10 15:05:13 +0000

    Update DESCRIPTION::description to place C++ within quotes.

--- R/golden-package.R
+++ R/golden-package.R
@@ -1,6 +1,6 @@
 #' Golden: Framework for Patient-Level Microsimulation of Risk Factor Trajectories & Hazard-Based Events
 #'
-#' Fast, flexible, patient-level microsimulation. Time-stepped simulation with a C++ back-end from user-supplied initial population, trajectories, hazards, and corresponding event transitions.
+#' Fast, flexible, patient-level microsimulation. Time-stepped simulation with a 'C++' back-end from user-supplied initial population, trajectories, hazards, and corresponding event transitions.
 #' User-defined aggregate time series histories are returned together with the final population. Designed for simulation of chronic diseases with continuous and evolving risk factors, but could easily be applied more generally.
 #'
 #' @docType package
