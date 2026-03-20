37065487bfe7e80f30a33c29185ca2c947816cce
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2026-03-20 14:18:53 +0000

    Update data docs (a start)

--- R/data.R
+++ R/data.R
@@ -1,12 +1,45 @@
 #' Example BMI distribution
 #'
-#' details TODO
-#' @format
-#' TODO
+#'
+#'
+#' @description Description of the bmi_fits dataset goes here
+#'
+#' @format A data.frame with 28 rows and 4 variables:
 #' \describe{
-#'   \item{foo}{bar}
+#'   \item{sex}{
+#'
+#' | *Type:*        | character       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for sex goes here              |
+#'
+#'   }
+#'   \item{acat}{
+#'
+#' | *Type:*        | character       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for acat goes here              |
+#'
+#'   }
+#'   \item{k}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for k goes here              |
+#'
+#'   }
+#'   \item{theta}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for theta goes here              |
+#'
+#'   }
 #' }
-#' @source TODO
+#' @source Where the data came from
 #' @name bmi_fits
 #' @docType data
 #' @export
@@ -14,13 +47,157 @@
 
 #' Example globorisk coefficients
 #'
-#' details TODO
-#' @format
-#' TODO
+#'
+#' @description Description of the globorisk_coefs dataset goes here
+#'
+#' @format A data.frame with 1 rows and 18 variables:
 #' \describe{
-#'   \item{foo}{bar}
+#'   \item{type}{
+#'
+#' | *Type:*        | character       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for type goes here              |
+#'
+#'   }
+#'   \item{main_sbpc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_sbpc goes here              |
+#'
+#'   }
+#'   \item{main_tcc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_tcc goes here              |
+#'
+#'   }
+#'   \item{main__Idm_1}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main__Idm_1 goes here              |
+#'
+#'   }
+#'   \item{main_smok}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_smok goes here              |
+#'
+#'   }
+#'   \item{main_sexdm}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_sexdm goes here              |
+#'
+#'   }
+#'   \item{main_sexsmok}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_sexsmok goes here              |
+#'
+#'   }
+#'   \item{tvc_sbpc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for tvc_sbpc goes here              |
+#'
+#'   }
+#'   \item{tvc_tcc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for tvc_tcc goes here              |
+#'
+#'   }
+#'   \item{tvc_dm}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for tvc_dm goes here              |
+#'
+#'   }
+#'   \item{tvc_smok}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for tvc_smok goes here              |
+#'
+#'   }
+#'   \item{main_bmi5c}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_bmi5c goes here              |
+#'
+#'   }
+#'   \item{main_smokc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_smokc goes here              |
+#'
+#'   }
+#'   \item{main_sexsmokc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_sexsmokc goes here              |
+#'
+#'   }
+#'   \item{tvc_smokc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for tvc_smokc goes here              |
+#'
+#'   }
+#'   \item{tvc_bmi5c}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for tvc_bmi5c goes here              |
+#'
+#'   }
+#'   \item{main_sbpsexc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for main_sbpsexc goes here              |
+#'
+#'   }
+#'   \item{lac}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for lac goes here              |
+#'
+#'   }
 #' }
-#' @source TODO
+#' @source Where the data came from
 #' @name globorisk_coefs
 #' @docType data
 #' @export
@@ -28,13 +205,46 @@
 
 #' Example globorisk baseline hazard
 #'
-#' details TODO
-#' @format
-#' TODO
+#'
+#'
+#' @description Description of the globorisk_cvdr dataset goes here
+#'
+#' @format A data.frame with 20 rows and 4 variables:
 #' \describe{
-#'   \item{foo}{bar}
+#'   \item{agec}{
+#'
+#' | *Type:*        | integer       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for agec goes here              |
+#'
+#'   }
+#'   \item{sex}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for sex goes here              |
+#'
+#'   }
+#'   \item{cvd_0}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for cvd_0 goes here              |
+#'
+#'   }
+#'   \item{agesex}{
+#'
+#' | *Type:*        | character       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for agesex goes here              |
+#'
+#'   }
 #' }
-#' @source TODO
+#' @source Where the data came from
 #' @name globorisk_cvdr
 #' @docType data
 #' @export
@@ -42,13 +252,86 @@
 
 #' Example globorisk reference values
 #'
-#' details TODO
-#' @format
-#' TODO
+#'
+#'
+#' @description Description of the globorisk_rf dataset goes here
+#'
+#' @format A data.frame with 18 rows and 9 variables:
 #' \describe{
-#'   \item{foo}{bar}
+#'   \item{iso}{
+#'
+#' | *Type:*        | character       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for iso goes here              |
+#'
+#'   }
+#'   \item{agec}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for agec goes here              |
+#'
+#'   }
+#'   \item{sex}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for sex goes here              |
+#'
+#'   }
+#'   \item{mean_sbp}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mean_sbp goes here              |
+#'
+#'   }
+#'   \item{mean_tc}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mean_tc goes here              |
+#'
+#'   }
+#'   \item{mean_dm}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mean_dm goes here              |
+#'
+#'   }
+#'   \item{mean_smk}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mean_smk goes here              |
+#'
+#'   }
+#'   \item{mean_bmi}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mean_bmi goes here              |
+#'
+#'   }
+#'   \item{agesex}{
+#'
+#' | *Type:*        | character       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for agesex goes here              |
+#'
+#'   }
 #' }
-#' @source TODO
+#' @source Where the data came from
 #' @name globorisk_rf
 #' @docType data
 #' @export
@@ -56,13 +339,52 @@
 
 #' Example life table data
 #'
-#' details TODO
-#' @format
-#' TODO
+#' @description Description of the lifetable_data dataset goes here
+#'
+#' @format A data.frame with 10,201 rows and 5 variables:
 #' \describe{
-#'   \item{foo}{bar}
+#'   \item{year}{
+#'
+#' | *Type:*        | integer       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for year goes here              |
+#'
+#'   }
+#'   \item{age}{
+#'
+#' | *Type:*        | integer       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for age goes here              |
+#'
+#'   }
+#'   \item{mxM}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mxM goes here              |
+#'
+#'   }
+#'   \item{mxF}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mxF goes here              |
+#'
+#'   }
+#'   \item{mxB}{
+#'
+#' | *Type:*        | numeric       |
+#' | -------------- | ------------- |
+#' |                |               |
+#' | *Description:* | Description for mxB goes here              |
+#'
+#'   }
 #' }
-#' @source TODO
+#' @source Where the data came from
 #' @name lifetable_data
 #' @docType data
 #' @export
@@ -70,6 +392,8 @@
 
 #' Example population structure
 #'
+#' Notes 101 rows, 2 cols popM, popF; matrix
+#' 
 #' details TODO
 #' @format
 #' TODO
